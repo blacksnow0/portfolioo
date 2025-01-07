bdd218aca56d0526ffa352598ef9cf11ca39c42f
blacksnow0 <86392451+blacksnow0@users.noreply.github.com>
2025-01-07 21:41:34 +0530

    new project added

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import project1 from "../Assets/project1.png";
 import project2 from "../Assets/project2.png";
 import project3 from "../Assets/project3.png";
+import project4 from "../Assets/project4.png";
 
 function Projects() {
   return (
@@ -15,16 +16,40 @@ function Projects() {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
         <div className="relative group">
-          <div className="overflow-hidden  shadow-lg">
+          <div className="overflow-hidden rounded-lg shadow-lg">
+            <img
+              src={project4}
+              alt="Project 4"
+              className="w-full h-48 object-center transition-transform duration-300 group-hover:scale-105"
+            />
+          </div>
+          <div className="absolute inset-0 rounded-lg bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center">
+            <h3 className="text-2xl font-semibold text-offWhite drop-shadow-xl">
+              RoadWays
+            </h3>
+
+            <a
+              href="https://roadways-drab.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="mt-4 px-6 py-2 bg-red-400 text-white font-bold rounded-lg shadow-md hover:bg-red-500"
+            >
+              View Details
+            </a>
+          </div>
+        </div>
+
+        <div className="relative group">
+          <div className="overflow-hidden rounded-lg shadow-lg">
             <img
               src={project1}
               alt="Project 1"
-              className="w-full h-64 object-cover transition-transform duration-300 group-hover:scale-105"
+              className="w-full h-48 object-center transition-transform duration-300 group-hover:scale-105"
             />
           </div>
-          <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center">
+          <div className="absolute inset-0 rounded-lg bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center">
             <h3 className="text-2xl font-semibold text-offWhite drop-shadow-xl">
               BurnMetric
             </h3>
@@ -41,14 +66,14 @@ function Projects() {
         </div>
 
         <div className="relative group">
-          <div className="overflow-hidden  shadow-lg">
+          <div className="overflow-hidden rounded-lg shadow-lg">
             <img
               src={project2}
               alt="Project 2"
-              className="w-full h-64 object-cover transition-transform duration-300 group-hover:scale-105"
+              className="w-full h-48 object-center transition-transform duration-300 group-hover:scale-105"
             />
           </div>
-          <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center">
+          <div className="absolute inset-0 rounded-lg bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center">
             <h3 className="text-2xl font-semibold text-offWhite drop-shadow-xl">
               Chatorita
             </h3>
@@ -65,14 +90,14 @@ function Projects() {
         </div>
 
         <div className="relative group">
-          <div className="overflow-hidden  shadow-lg">
+          <div className="overflow-hidden rounded-lg shadow-lg">
             <img
               src={project3}
               alt="Project 3"
-              className="w-full h-64 object-cover transition-transform duration-300 group-hover:scale-105"
+              className="w-full h-48 object-center transition-transform duration-300 group-hover:scale-105"
             />
           </div>
-          <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center">
+          <div className="absolute inset-0 rounded-lg bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center">
             <h3 className="text-2xl font-semibold text-offWhite drop-shadow-xl">
               New Urvashi
             </h3>
