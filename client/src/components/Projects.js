40e1887b4963b8fae6bf055fb7daaa1f8d2280d0
blacksnow0 <86392451+blacksnow0@users.noreply.github.com>
2025-01-12 14:29:01 +0530

    ui updated

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,117 +1,104 @@
+/* Slick Carousel Styles */
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import React from "react";
+import Slider from "react-slick";
 import project1 from "../Assets/project1.png";
 import project2 from "../Assets/project2.png";
 import project3 from "../Assets/project3.png";
 import project4 from "../Assets/project4.png";
 
 function Projects() {
+  const projects = [
+    {
+      img: project4,
+      title: "RoadWays",
+      link: "https://roadways-drab.vercel.app/",
+    },
+    {
+      img: project1,
+      title: "BurnMetrics",
+      link: "https://burn-metrics.vercel.app/",
+    },
+    {
+      img: project2,
+      title: "Chatorita",
+      link: "https://chatorita.vercel.app/",
+    },
+    {
+      img: project3,
+      title: "New Urvashi",
+      link: "https://newurvashibadrinath.com",
+    },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024, // Tablets and below
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768, // Mobile devices
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <section className="bg-customGreen text-white py-20 px-6 md:px-20">
-      <div className="text-center mb-16">
-        <h1 className="text-5xl font-bold font-serif text-offWhite drop-shadow-xl tracking-widest">
-          My Projects
+    <section className="py-20 px-8 md:px-16 lg:px-24 bg-customGreen">
+      <div className="text-center mb-14">
+        <h1 className="text-5xl font-extrabold text-white">
+          Featured Projects
         </h1>
-        <p className="mt-4 text-lg text-offWhite">
-          A showcase of my journey through code and creativity.
+        <p className="mt-4 text-lg text-gray-200">
+          Explore a curated collection of my most innovative and impactful work.
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
-        <div className="relative group">
-          <div className="overflow-hidden rounded-lg shadow-lg">
-            <img
-              src={project4}
-              alt="Project 4"
-              className="w-full h-48 object-center transition-transform duration-300 group-hover:scale-105"
-            />
+      <Slider {...settings} className="relative">
+        {projects.map((project, index) => (
+          <div key={index} className="px-4">
+            <div className="group relative bg-gray-900 rounded-lg overflow-hidden hover:shadow-2xl transition-shadow duration-500">
+              {/* Image Section */}
+              <div className="relative overflow-hidden p-1">
+                <img
+                  src={project.img}
+                  alt={project.title}
+                  className="rounded-lg w-full h-56 object-cover transform transition-transform duration-500 group-hover:scale-105"
+                />
+                <div className="absolute inset-0 bg-gray-900/40 opacity-80 group-hover:opacity-100 transition-opacity duration-500"></div>
+              </div>
+              {/* Content Section */}
+              <div className="p-6">
+                <h3 className="text-xl font-bold text-white text-center group-hover:text-yellow-600 transition-colors duration-300">
+                  {project.title}
+                </h3>
+                <div className="mt-4 text-center">
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="inline-block px-6 py-2 bg-yellow-600 text-white font-semibold rounded-full shadow-lg hover:bg-yellow-600 transition-transform transform hover:-translate-y-1"
+                  >
+                    View Project
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="absolute inset-0 rounded-lg bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center">
-            <h3 className="text-2xl font-semibold text-offWhite drop-shadow-xl">
-              RoadWays
-            </h3>
-
-            <a
-              href="https://roadways-drab.vercel.app/"
-              target="_blank"
-              rel="noreferrer"
-              className="mt-4 px-6 py-2 bg-red-400 text-white font-bold rounded-lg shadow-md hover:bg-red-500"
-            >
-              View Details
-            </a>
-          </div>
-        </div>
-
-        <div className="relative group">
-          <div className="overflow-hidden rounded-lg shadow-lg">
-            <img
-              src={project1}
-              alt="Project 1"
-              className="w-full h-48 object-center transition-transform duration-300 group-hover:scale-105"
-            />
-          </div>
-          <div className="absolute inset-0 rounded-lg bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center">
-            <h3 className="text-2xl font-semibold text-offWhite drop-shadow-xl">
-              BurnMetric
-            </h3>
-
-            <a
-              href="https://burn-metrics.vercel.app/"
-              target="_blank"
-              rel="noreferrer"
-              className="mt-4 px-6 py-2 bg-red-400 text-white font-bold rounded-lg shadow-md hover:bg-red-500"
-            >
-              View Details
-            </a>
-          </div>
-        </div>
-
-        <div className="relative group">
-          <div className="overflow-hidden rounded-lg shadow-lg">
-            <img
-              src={project2}
-              alt="Project 2"
-              className="w-full h-48 object-center transition-transform duration-300 group-hover:scale-105"
-            />
-          </div>
-          <div className="absolute inset-0 rounded-lg bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center">
-            <h3 className="text-2xl font-semibold text-offWhite drop-shadow-xl">
-              Chatorita
-            </h3>
-
-            <a
-              href="https://chatorita.vercel.app/"
-              target="_blank"
-              rel="noreferrer"
-              className="mt-4 px-6 py-2 bg-red-400 text-white font-bold rounded-lg shadow-md hover:bg-red-500"
-            >
-              View Details
-            </a>
-          </div>
-        </div>
-
-        <div className="relative group">
-          <div className="overflow-hidden rounded-lg shadow-lg">
-            <img
-              src={project3}
-              alt="Project 3"
-              className="w-full h-48 object-center transition-transform duration-300 group-hover:scale-105"
-            />
-          </div>
-          <div className="absolute inset-0 rounded-lg bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center">
-            <h3 className="text-2xl font-semibold text-offWhite drop-shadow-xl">
-              New Urvashi
-            </h3>
-            <a
-              href="https://newurvashibadrinath.com"
-              target="_blank"
-              rel="noreferrer"
-              className="mt-4 px-6 py-2 bg-red-400 text-white font-bold rounded-lg shadow-md hover:bg-red-500"
-            >
-              View Details
-            </a>
-          </div>
-        </div>
-      </div>
+        ))}
+      </Slider>
     </section>
   );
 }
